e7bed13499dc76d5c3f2093406f5a9e691f6b3ca
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-04-29 17:06:46 +0530

    Set accounts app and Register, Login Templates

--- btre/settings.py
+++ btre/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',    
-    'realtors.apps.RealtorsConfig',    
+    'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
