2ca2f5b72915e9a960512dd038b077271dd5f6aa
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-04-29 18:32:43 +0530

    Add User Login View

--- accounts/views.py
+++ accounts/views.py
@@ -35,23 +35,35 @@ def register(request):
                     user.save()
                     messages.success(request, 'You are now registered.')
                     return redirect('login')
-
         else:
             messages.error(request, "Passwords don't match")
             return redirect('register')
-
-        # 
     else:
         return render(request, 'accounts/register.html')
 
+
 def login(request):
     if request.method == 'POST':
-        # Login User
-        return
-    return render(request, 'accounts/login.html')
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credenials')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
 
 def logout(request):
     return redirect('index')
+
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
