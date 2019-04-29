6b518b6496f147b541d4700727979f9b1d1c40c5
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-04-30 00:09:53 +0530

    Add Email config

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,10 +14,28 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+
+        # Check if user has made an enquiry already
+        if request.user.is_authenticated:
+                user_id = request.user.id
+                has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+                if has_contacted:
+                        messages.error(request, "You've already made an enquiry for this listing")
+                        return redirect('/listings/'+listing_id)
+
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name,
         email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        send_mail(
+                'Property Listing Enquery',
+                'There has been enquiry for '+ listing + '.',
+                'gauniyalmohit@gmail.com',
+                ['mohitgauniyal19a@gmail.com'],
+                fail_silently = False
+        )
 
         messages.success(request, 'Your request has been submitted, Realtor will get back to you soon')
 
