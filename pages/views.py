f8c76f03965f446c4ab173753abef5b90f5e625f
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-04-29 07:34:16 +0530

    Add Search Form Choices

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 from realtors.models import Realtor
+
+
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    context = {'listings' : listings}
+    context = {
+        'listings' : listings,
+        'state_choices' : state_choices,
+        'bedroom_choices' : bedroom_choices,
+        'price_choices' : price_choices,
+        }
 
     return render(request, 'pages/index.html', context)
 
